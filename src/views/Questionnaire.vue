e2569aac8b83b6aba7c31d3f9d66f353eb3fe119
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-25 00:25:39 +0900

    Update Questionnaire.vue
    各質問の回答をvuexに対応

--- src/views/Questionnaire.vue
+++ src/views/Questionnaire.vue
@@ -2,26 +2,28 @@
   <div class="questionnaire">
     <p>STEP2</p>
     <h3>お客様の情報を入力してください</h3>
-    <div>
-      <p>現在、生命保険に加入されていますか？</p>
-      <form>
-        <label><input type="radio" name="q1" v-model="q1Value" value="yes">はい</label>
-        <label><input type="radio" name="q1" v-model="q1Value" value="no">いいえ</label>
-      </form>
-    </div>
-    <div v-if="q1Value">
-      <p>現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？</p>
-      <form>
-        <label><input type="radio" name="q2" v-model="q2Value" value="yes">はい</label>
-        <label><input type="radio" name="q2" v-model="q2Value" value="no">いいえ</label>
-      </form>
-    </div>
-    <div v-if="q2Value">
-      <p>過去５年以内に、病院やけがで、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？</p>
-      <form>
-        <label><input type="radio" name="q3" v-model="q3Value" value="yes">はい</label>
-        <label><input type="radio" name="q3" v-model="q3Value" value="no">いいえ</label>
-      </form>
+    <div @change="answer()">
+      <div>
+        <p>現在、生命保険に加入されていますか？</p>
+        <form>
+          <label><input type="radio" name="q1" v-model="q1Value" value="はい">はい</label>
+          <label><input type="radio" name="q1" v-model="q1Value" value="いいえ">いいえ</label>
+        </form>
+      </div>
+      <div v-if="q1Value">
+        <p>現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？</p>
+        <form>
+          <label><input type="radio" name="q2" v-model="q2Value" value="はい">はい</label>
+          <label><input type="radio" name="q2" v-model="q2Value" value="いいえ">いいえ</label>
+        </form>
+      </div>
+      <div v-if="q2Value">
+        <p>過去５年以内に、病院やけがで、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？</p>
+        <form>
+          <label><input type="radio" name="q3" v-model="q3Value" value="はい">はい</label>
+          <label><input type="radio" name="q3" v-model="q3Value" value="いいえ">いいえ</label>
+        </form>
+      </div>
     </div>
     <nav>
       <router-link to="/"><button>戻る</button></router-link>
@@ -38,6 +40,24 @@ export default {
       q1Value: '',
       q2Value: '',
       q3Value: ''
+    }
+  },
+  methods: {
+    answer: function(){
+      this.$store.state.infoAnswer1 = this.q1Value;
+      this.$store.state.infoAnswer2 = this.q2Value;
+      this.$store.state.infoAnswer3 = this.q3Value;
+    }
+  },
+  computed: {
+    infoAnswer1(){
+      return this.$store.state.infoAnswer1;
+    },
+    infoAnswer2(){
+      return this.$store.state.infoAnswer2;
+    },
+    infoAnswer3(){
+      return this.$store.state.infoAnswer3;
     }
   }
 }
