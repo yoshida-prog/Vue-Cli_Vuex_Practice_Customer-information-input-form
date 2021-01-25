caccca9453a4b7ec118d792660702c21b352434d
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-26 04:11:14 +0900

    Update index.js
    gettersとmutationsを追加

--- src/store/index.js
+++ src/store/index.js
@@ -12,7 +12,33 @@ export default new Vuex.Store({
     infoAnswer3: '',
     consultation: ''
   },
+  getters: {
+    userSexState: state => state.userSex,
+    userBirthdayState: state => state.userBirthday,
+    infoAnswer1State: state => state.infoAnswer1,
+    infoAnswer2State: state => state.infoAnswer2,
+    infoAnswer3State: state => state.infoAnswer3,
+    consultationText: state => state.consultation
+  },
   mutations: {
+    changeUserSex(state, sex){
+      state.userSex = sex
+    },
+    changeUserBirthday(state, birthday){
+      state.userBirthday = birthday
+    },
+    changeUserInfoAnswer1(state, infoAnswer1){
+      state.infoAnswer1 = infoAnswer1
+    },
+    changeUserInfoAnswer2(state, infoAnswer2){
+      state.infoAnswer2 = infoAnswer2
+    },
+    changeUserInfoAnswer3(state, infoAnswer3){
+      state.infoAnswer3 = infoAnswer3
+    },
+    changeConsultationText(state, consultation){
+      state.consultation = consultation
+    }
   },
   actions: {
   },
