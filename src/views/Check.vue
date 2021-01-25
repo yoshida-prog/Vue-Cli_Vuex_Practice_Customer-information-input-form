9a33751c31e553eb2a20ba5c702d02a644f8a543
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-26 04:18:32 +0900

    Update Check.vue

--- src/views/Check.vue
+++ src/views/Check.vue
@@ -3,17 +3,17 @@
     <p>STEP4</p>
     <h3>以下の内容をご確認ください</h3>
     <h4>性別</h4>
-    <p>{{ userSex }}</p>
+    <p>{{ userSexState }}</p>
     <h4>生年月日</h4>
-    <p>{{ userBirthday }}</p>
+    <p>{{ userBirthdayState }}</p>
     <h4>現在、生命保険に加入されていますか？</h4>
-    {{ infoAnswer1 }}
+    {{ infoAnswer1State }}
     <h4>現在入院中ですか。または、最近３ヶ月以内に医師の診察・検査の結果、入院・手術をすすめられたことはありますか？</h4>
-    {{ infoAnswer2 }}
+    {{ infoAnswer2State }}
     <h4>過去５年以内に、病院やけがで、手術をうけたことまたは継続して７日以上の入院をしたことがありますか？</h4>
-    {{ infoAnswer3 }}
+    {{ infoAnswer3State }}
     <h4>ご相談内容</h4>
-    {{ consultation }}
+    {{ consultationText }}
     <nav>
       <router-link to="/Consultation"><button>戻る</button></router-link>
       <button>進む</button>
@@ -22,27 +22,19 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
     name: 'check',
     computed: {
-      userSex(){
-        return this.$store.state.userSex;
-      },
-      userBirthday(){
-        return this.$store.state.userBirthday;
-      },
-      infoAnswer1(){
-        return this.$store.state.infoAnswer1;
-      },
-      infoAnswer2(){
-        return this.$store.state.infoAnswer2;
-      },
-      infoAnswer3(){
-        return this.$store.state.infoAnswer3;
-      },
-      consultation(){
-        return this.$store.state.consultation;
-      }
+      ...mapGetters([
+        'userSexState',
+        'userBirthdayState',
+        'infoAnswer1State',
+        'infoAnswer2State',
+        'infoAnswer3State',
+        'consultationText'
+      ])
     }
   }
 </script>
