75a788f76ed0f3b5b47eac8aa10e99a43c7ab7d4
よしだ <56685063+yoshida-prog@users.noreply.github.com>
2021-01-26 04:13:56 +0900

    Update Consultation.vue

--- src/views/Consultation.vue
+++ src/views/Consultation.vue
@@ -3,36 +3,6 @@
     <p>STEP3</p>
     <h3>ご相談内容をご記入ください</h3>
     <p>ご相談内容</p>
-    <textarea rows="8" cols="80"></textarea>
-    <nav>
-      <router-link to="/Questionnaire"><button>戻る</button></router-link>
-      <button>進む</button>
-    </nav>
-  </div>
-</template>
-
-<script>
-  export default {
-    name: 'consultations',
-    data: () => {
-      return{
-
-      }
-    }
-  }
-</script>
-
-<style>
-  button{
-    width: 70px;
-    height: 40px;
-  }
-</style>
-<template>
-  <div class="consultation">
-    <p>STEP3</p>
-    <h3>ご相談内容をご記入ください</h3>
-    <p>ご相談内容</p>
     <textarea rows="8" cols="80" v-model="textContent" @input="checkText"></textarea>
     <nav>
       <router-link to="/Questionnaire"><button>戻る</button></router-link>
@@ -42,6 +12,8 @@
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
     name: 'consultations',
     data: () => {
@@ -51,13 +23,13 @@
     },
     methods: {
       checkText: function(){
-        this.$store.state.consultation = this.textContent;
+        this.$store.commit('changeConsultationText', this.textContent);
       }
     },
     computed: {
-      consultation(){
-        return this.$store.state.consultation;
-      }
+      ...mapGetters([
+        'consultationText'
+      ])
     }
   }
 </script>
